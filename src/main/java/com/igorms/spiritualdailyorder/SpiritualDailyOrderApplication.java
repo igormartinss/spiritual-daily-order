44a7ae1ad2217b18044fa175ddca3ef8295a5064
Igor Martins <igor.martins.silverio@gmail.com>
2021-11-29 23:23:30 -0300

    spring boot configurations

---
+++ src/main/java/com/igorms/spiritualdailyorder/SpiritualDailyOrderApplication.java
@@ -0,0 +1,13 @@
+package com.igorms.spiritualdailyorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritualDailyOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiritualDailyOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/igorms/spiritualdailyorder/SpiritualDailyOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorms.spiritualdailyorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiritualDailyOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
